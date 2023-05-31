c35736a6091112af7f1f18952567aeba2e90da06
Femi Fakoya <oluwafemilopo@gmail.com>
2023-05-31 17:45:35 +0100

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+access_key = "AKIAS2RRCSVWIX7H7Y5V"
+secret_key = "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"
+instance_type = "t2.medium"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+    type = string
+}
+
+variable "secret_key" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
